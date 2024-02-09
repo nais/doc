37fef504c06a5e70b60b3e9f643b055c7c42c1d5
Thomas Krampl <85170275+thokra-nav@users.noreply.github.com>
2024-02-09 10:46:54 +0100

    Allow user to specify automatic redirect (#597)
    This feature adds a button to the main page of the generic doc which
    allows the user to configure a custom organization documentation to
    redirect to when visiting the generic doc.
    
    When this is configured, the user will be presented with a dialog
    explaining that the user will be redirected, and the user can cancel the
    redirect.
    
    This is to prevent misconfiguration and permanent bad redirects.

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ def define_env(env):
     @env.macro
     def if_tenant():
         if tenant() == 'tenant':
-            return '!!! info\n' + \
-                '     This is a generic version of the NAIS documentation available to the public.<br> If you are currently using NAIS, go to the documentation tailored for your organization.<br> https://doc.\<org\>.cloud.nais.io'
-
+           return '!!! info\n' + \
+            '     This is a generic version of the NAIS documentation available to the public.<br> '+ \
+            'If you are currently using NAIS, go to the documentation tailored for your organization.<br> '+ \
+            'https://doc.&lt;org&gt;.cloud.nais.io<br>'+ \
+            '<button data-action="configure-redirect" class="md-button md-button--primary">Configure automatic redirect</button>'
         else:
             return ''
