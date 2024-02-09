bd2546754f6c1933d7d6e3047c7ae8c591a3fdea
frodesundby <frode.sundby@nav.no>
2024-02-09 09:01:43 +0100

    add build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+rm -rf ./out ./docs-base
+mkdir -p ./out ./docs-base
+
+# Copy documentation to base folder as we need to use docs as a staging folder
+cp -r ./docs/* ./docs-base
+
+for TENANT in $@; 
+  do 
+    echo $TENANT
+    rm -rf ./docs
+    cp -r ./docs-base ./docs
+    cp -rf ./tenants/$TENANT/* ./docs
+    TENANT=$TENANT poetry run mkdocs build -d out/$TENANT
+  done
+
