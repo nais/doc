c8f6574e7b19ae77d376832e4c4aa5b486f7a103
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2024-05-15 11:01:56 +0200

    build: include dotfiles as well
    Co-authored-by: Johnny Horvi <johnny@horvi.no>

--- build.sh
+++ build.sh
@@ -5,12 +5,12 @@ rm -rf ./out ./docs-base
 mkdir -p ./out ./docs-base
 
 # Copy documentation to base folder as we need to use docs as a staging folder
-cp -r ./docs/* ./docs-base
+cp -ra ./docs/. ./docs-base/
 
 for TENANT in $@;
   do
     rm -rf ./docs
-    cp -r ./docs-base ./docs
+    cp -ra ./docs-base/. ./docs/
     cp -rf ./tenants/$TENANT/* ./docs || true
     TENANT=$TENANT poetry run mkdocs build --strict -d out/$TENANT
   done
