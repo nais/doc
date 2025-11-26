270bf3a34c3b5cacff25732bab18ab3961d4ccf9
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2025-11-26 01:50:14 +0100

    def

---
+++ svdoc/src/lib/renderers/Def.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	interface DefToken {
+		type: "def";
+		tag: string;
+		raw: string;
+		href: string;
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	let { token }: { token: DefToken } = $props();
+
+	// Link reference definitions don't render any visible content
+	// They are used as metadata for reference-style links [text][tag]
+</script>
