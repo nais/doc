cbf59c90434f5ad04dc8d4ee7fd145ac1f5b990d
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2025-11-26 13:28:17 +0100

    Assets and some other stuff

---
+++ svdoc/src/lib/renderers/DefinitionList.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+	import type { Token } from "marked";
+	import Renderer from "./Renderer.svelte";
+
+	interface DefinitionToken {
+		tokens: Token[];
+	}
+
+	interface DefinitionListItemToken {
+		termTokens: Token[];
+		definitions: DefinitionToken[];
+	}
+
+	interface Props {
+		token: {
+			items: DefinitionListItemToken[];
+		};
+	}
+
+	let { token }: Props = $props();
+</script>
+
+<dl class="definition-list">
+	{#each token.items as item, i (i)}
+		<dt class="definition-term">
+			<Renderer tokens={item.termTokens} />
+		</dt>
+		{#each item.definitions as definition, j (j)}
+			<dd class="definition-description">
+				<Renderer tokens={definition.tokens} />
+			</dd>
+		{/each}
+	{/each}
+</dl>
+
+<style>
+	.definition-list {
+		margin: 1rem 0;
+	}
+
+	.definition-term {
+		font-weight: 600;
+		margin-top: 1rem;
+	}
+
+	.definition-term:first-child {
+		margin-top: 0;
+	}
+
+	/* Style inline code in terms */
+	.definition-term :global(code) {
+		font-size: 0.95em;
+		padding: 0.15rem 0.4rem;
+		background-color: var(--ax-bg-neutral-soft, rgba(175, 184, 193, 0.15));
+		border-radius: 0.25rem;
+		font-weight: 500;
+	}
+
+	.definition-description {
+		margin-left: 1.5rem;
+		margin-top: 0.25rem;
+		color: var(--ax-text-neutral, inherit);
+	}
+
+	/* Remove margin from first paragraph in definition */
+	.definition-description :global(p:first-child) {
+		margin-top: 0;
+	}
+
+	/* Style for Required/Optional badges */
+	.definition-description :global(strong) {
+		color: var(--ax-text-neutral, inherit);
+	}
+</style>
