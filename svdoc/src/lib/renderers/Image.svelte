6c27694583ef4f9ea5f413fa5fa08df428e6973b
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2025-12-02 14:02:12 +0100

    emoji support

--- svdoc/src/lib/renderers/Image.svelte
+++ svdoc/src/lib/renderers/Image.svelte
@@ -2,25 +2,11 @@
 	import type { Tokens } from "marked";
 
 	let { token }: { token: Tokens.Image } = $props();
-
-	// Check if this is an emoji (twemoji pattern)
-	const isEmoji = token.href.includes("twemoji") || token.text.startsWith(":");
 </script>
 
-{#if isEmoji}
-	<img src={token.href} alt={token.text} title={token.title || token.text} class="twemoji" />
-{:else}
-	<img src={token.href} alt={token.text} title={token.title} />
-{/if}
+<img src={token.href} alt={token.text} title={token.title} />
 
 <style>
-	.twemoji {
-		display: inline;
-		height: 1.25em;
-		width: 1.25em;
-		vertical-align: text-bottom;
-	}
-
 	img {
 		max-width: 100%;
 		height: auto;
--- svdoc/src/lib/types/emoji-name-map.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "emoji-name-map" {
-	const emojiNameMap: {
-		get(name: string): string | undefined;
-		emoji: Record<string, string>;
-	};
-	export default emojiNameMap;
-}
