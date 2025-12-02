7ff6b65eb3903342ec8347b88e5f25385db63529
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2025-12-02 10:35:46 +0100

    Fix some cards

--- svdoc/src/lib/renderers/Footnote.svelte
+++ svdoc/src/lib/renderers/Footnote.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { Token } from "marked";
-	import ContentRenderer from "./ContentRenderer.svelte";
+	import Renderer from "./Renderer.svelte";
 
 	type FootnoteToken = {
 		// type: "footnote";
@@ -14,7 +14,7 @@
 </script>
 
 <li id="footnote-{token.id}">
-	<ContentRenderer tokens={token.tokens} />
+	<Renderer tokens={token.tokens} />
 	<a
 		href="#footnote-ref-{token.id}"
 		data-footnote-backref
