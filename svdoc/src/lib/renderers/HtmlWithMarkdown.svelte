5af946897f8ffc30d83d4a0bcec949c36fc1c5ff
Thomas Krampl <thomas.siegfried.krampl@nav.no>
2026-03-02 09:05:51 +0100

    Upgrade deps, fix lint and check

--- svdoc/src/lib/renderers/HtmlWithMarkdown.svelte
+++ svdoc/src/lib/renderers/HtmlWithMarkdown.svelte
@@ -14,16 +14,22 @@
 	let { token }: { token: HtmlWithMarkdownToken } = $props();
 
 	// Extract tag name from openTag (e.g., '<div class="grid cards">' -> 'div')
-	const tagMatch = token.openTag.match(/^<(\w+)/);
-	const tagName = tagMatch ? tagMatch[1] : "div";
+	const tagName = $derived.by(() => {
+		const tagMatch = token.openTag.match(/^<(\w+)/);
+		return tagMatch ? tagMatch[1] : "div";
+	});
 
 	// Extract class from openTag (e.g., '<div class="grid cards">' -> 'grid cards')
-	const classMatch = token.openTag.match(/class="([^"]*)"/);
-	const className = classMatch ? classMatch[1] : undefined;
+	const className = $derived.by(() => {
+		const classMatch = token.openTag.match(/class="([^"]*)"/);
+		return classMatch ? classMatch[1] : undefined;
+	});
 
 	// Extract id from openTag
-	const idMatch = token.openTag.match(/id="([^"]*)"/);
-	const id = idMatch ? idMatch[1] : undefined;
+	const id = $derived.by(() => {
+		const idMatch = token.openTag.match(/id="([^"]*)"/);
+		return idMatch ? idMatch[1] : undefined;
+	});
 </script>
 
 <svelte:element this={tagName} class={className} {id}>
