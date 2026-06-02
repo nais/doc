d17ac3a9e60529141e283f6ac6d4eb1d71c42468
Kyrre Havik <kyrre.havik@nav.no>
2026-06-02 10:42:13 +0200

    fix: trenger ikke være bestemt form

--- svdoc/src/lib/components/PageMeta.svelte
+++ svdoc/src/lib/components/PageMeta.svelte
@@ -96,7 +96,7 @@
 
 			<div class="meta-item">
 				<a href="https://uustatus.no/nb/erklaringer/publisert/004cbdfd-7d16-40e5-9a78-09757c92b824" target="_blank" rel="noopener noreferrer" class="source-link">
-					Tilgjengelighetserklæringen
+					Tilgjengelighetserklæring
 				</a>
 			</div>
 		</div>
